2f2d2a66005ff16997d44472592fc7f8ba6125dc
EC2 Default User <ec2-user@ip-172-31-89-27.ec2.internal>
2022-02-14 05:51:31 +0000

    新規商品ケーキ追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,25 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+     @item = Item.new(params_path)
+     @item.save
+     redirect_to root_path
+
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def params_path
+     params.require(:item).permit(:name, :introduction)
   end
 end
