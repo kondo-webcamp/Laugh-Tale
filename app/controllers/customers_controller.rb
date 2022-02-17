3887b2649a7d5b0efb840c2f151008e41d9ea173
EC2 Default User <ec2-user@ip-172-31-89-27.ec2.internal>
2022-02-17 07:41:13 +0000

    会員情報編集

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -22,4 +22,5 @@ class CustomersController < ApplicationController
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 
+  
 end
