703e828302a7387096ea23b5bd5094b1caba5028
EC2 Default User <ec2-user@ip-172-31-89-27.ec2.internal>
2022-02-18 05:15:23 +0000

    退会機能追加

---
+++ app/javascript/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.customer-modal0').click(function(){
+    $('.customer-modal').fadeIn();
+  });
+
+  $('.customer-modal2').click(function(){
+    $('.customer-modal').fadeOut();
+  });
+});
