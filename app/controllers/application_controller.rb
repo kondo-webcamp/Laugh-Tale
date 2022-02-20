7a4364c7544c54c913550549741fdf4aa5359ee2
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-20 06:51:37 +0000

    カート機能リンク追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     root_path(resource)
   end
   def after_sign_up_path_for(resource)
-    root_path
+    root_path(resource)
   end
   def after_sign_out_path_for(resource)
     root_path(resource)
